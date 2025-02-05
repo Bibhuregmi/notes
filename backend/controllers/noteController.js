53c12a60e208d8544440681f8fb1993d68cba4ea
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-02-05 16:49:24 -0500

    Added model and methods to routers to interact with db

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -1,26 +1,54 @@
 import expressAsyncHandler from "express-async-handler" //using this so that we can keep code modular and no need to use try catch block and next to handle error
-
+import { Note } from "../models/noteModels.js"
 // @desc : GET all the notes of the user
 // @route : GET api/notes
 export const getNotes = expressAsyncHandler(async (req, res) => {
-    res.status(200).json({message : 'All notes'})
+    const note = await Note.find(); 
+    res.status(200).json(note)
 })
 // @desc : Create new note for the user
 // @route : POST api/notes
 export const createNotes = expressAsyncHandler(async (req, res) => {
-    if(!req.body.title){
+    const { title, content} = req.body; //specifing what will a request body contains
+
+    if (!title || !content){
         res.status(400)
-        throw new Error('Please include the title for the body')
+        throw new Error('Plese include title and the content for the note')
     }
-    res.status(200).json({message : 'Create new notes'})
+    const newNote = await Note.create({
+        title: req.body.title,
+        content: req.body.content
+    }); 
+    
+    res.status(200).json(newNote);
 })
 // @desc : Update the notes of the user
 // @route : PUT api/notes/:id
 export const updateNotes = expressAsyncHandler(async (req, res) => {
-    res.status(200).json({message : `Update note of id ${req.params.id}`})
+    const id = req.params.id;   
+    const newTitle = req.body.title;
+    const newContent = req.body.content; 
+
+    const note = await Note.findById(id); //extracting the id from the response 
+    if(!note){
+        res.status(400)
+        throw new Error('Invalid Id!')
+    }
+    const updatedNote = await Note.findByIdAndUpdate(id,
+                                                    {title: newTitle,
+                                                    content: newContent}, {new : true} ) 
+    console.log(req.body);
+    res.status(200).json(updatedNote)
 })
 // @desc : Delete note of the user
 // @route : DELETE api/notes/:id
 export const deleteNotes = expressAsyncHandler(async (req, res) => {
-    res.status(200).json({message : `Delete note of id ${req.params.id}`})
+    const id = req.params.id; 
+    const note = await Note.findById(id); 
+    if(!note){
+        res.status(400)
+        throw new Error("Invalid id!")
+    }
+    await Note.findByIdAndDelete(id);
+    res.status(200).json({'Deleted note id': id})
 })
