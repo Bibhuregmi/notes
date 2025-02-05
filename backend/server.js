6e2e9b90b725e4739ad47b0df9ec7cb920f15f0d
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-02-05 16:50:36 -0500

    Added connection to db

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express'; 
 import dotenv from 'dotenv/config'; //looks for the .env file and loads it automatically. 
 import { errorHandler } from './middlewares/errorHandler.js';
+import { connectDb } from './config/db.js';
 import routes from './routes/noteRoutes.js'
 
 const port = process.env.PORT
 const app = express(); 
+
+connectDb(); 
 
 //using the middleware that parses the input body request
 app.use(express.json())
