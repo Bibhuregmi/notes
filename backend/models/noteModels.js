a78c7a8453d5da9c95e9673dd9674fc2b78bcb76
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-02-05 16:50:05 -0500

    Defining the model of the db

---
+++ backend/models/noteModels.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'; 
+
+//creating a schema of the MongoDB, schema is a blueprint that defines how our document is structured in the database. 
+const noteSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt:{
+        type: Date, 
+        default: Date.now, 
+    }
+})
+//cerating and exporting the model of the database. Model is a compiled version of the schema that provides methods to interact with the database. 
+export const Note = mongoose.model('Note', noteSchema); 
