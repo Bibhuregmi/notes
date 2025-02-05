d8310871ad7fd9f6251043ead7a7b5bd8a0aaa76
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-02-04 21:46:11 -0500

    Changes after middleware and controller

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 import express from 'express'; 
-import dotenv from 'dotenv/config'; 
+import dotenv from 'dotenv/config'; //looks for the .env file and loads it automatically. 
+import { errorHandler } from './middlewares/errorHandler.js';
 import routes from './routes/noteRoutes.js'
 
 const port = process.env.PORT
 const app = express(); 
 
+//using the middleware that parses the input body request
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+
+//specifing the url and the router 
 app.use("/api/notes", routes);
+//using the custom error handler 
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server is running in ${port}`);
