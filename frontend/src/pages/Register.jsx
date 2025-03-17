59c6208a107339c308b6cc961967fa7c9e088244
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-03-17 18:30:57 -0400

    Registration page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,113 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { FaEnvelope, FaLock, FaUser } from 'react-icons/fa';
+import { validateform } from '../utils/validation';
+import { useNavigate } from 'react-router-dom';
+import Popup from '../components/Popup';
 
 const Register = () => {
+  const navigation = useNavigate();
+  const navigatetoLogin = () => {
+    setShowPopup(true);
+    setTimeout(() => {
+      setShowPopup(false);
+      navigation('/login')
+    }, 3000);
+  }
+  const[formData, setFormData] = useState({
+    name : '',
+    email: '', 
+    password: '',
+    confirmPassword: '', 
+})
+  const [errors, setErrors] = useState({});
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleChange = (e) => { //targeting the input field on the basis of name attribute and capturing the changes made 
+    setFormData({...formData, [e.target.name] : e.target.value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationErrors = validateform(formData);
+    setErrors(validationErrors); 
+
+    if(Object.keys(validationErrors).length === 0){
+      console.log('Form submitted sucessfully', formData); 
+      navigatetoLogin();
+      //TODO: Logic for the API call 
+    }
+  }
   return (
-    <div>Register</div>
+    <div className = 'mx-4 my-4 w-[80vwh] h-[90vh] flex  justify-center px-2 py-2'>
+        <div className = 'h-full max-w-1/2 border-2 rounded-lg shadow-lg bg-[#121212] text-white px-6 py-6'>
+          <div className = 'text-center max-w-full mb-14'>
+            <h3 className='sm:text-2xl md:text-4xl lg:text-6xl font-bold'>Welcome to Note.</h3>
+            <p className='sm:text-sm md:text-lg lg:text-xl font-light'>Create a new Account</p>
+          </div>
+            <form onSubmit={handleSubmit}>
+              <div className='relative mb-8'>
+                <input 
+                  type='text' 
+                  name='name'
+                  placeholder = 'Enter your name' 
+                  className='w-full bg-white rounded-md h-8 pl-15 pr-6 py-3 text-black'
+                  value={formData.name}
+                  onChange={handleChange}
+                  >
+                  </input>
+                {errors.name && <p className='text-red-500 text-[0.5rem] italic'>{errors.name}</p>}
+                <FaUser className='absolute left-3 top-1/2 transform -translate-y-1/2 text-black'/>
+              </div>
+              <div className='relative mb-8'>
+                <input 
+                  type='email' 
+                  name = 'email'
+                  placeholder = 'Enter your email' 
+                  className='w-full bg-white rounded-md h-8 pl-15 pr-6 py-3 text-black'
+                  value={formData.email}
+                  onChange={handleChange}
+                  >
+                  </input>
+                  {errors.email && <p className='text-red-500 text-[0.5rem] italic'>{errors.email}</p>}
+                <FaEnvelope className='absolute left-3 top-1/2 transform -translate-y-1/2 text-black'/>
+              </div>
+              <div className='relative mb-8'>
+                <input 
+                  type='password' 
+                  name = 'password'
+                  placeholder = 'Enter your password' 
+                  className='w-full bg-white rounded-md h-8 pl-15 pr-6 py-3 text-black'
+                  value={formData.password}
+                  onChange={handleChange}
+                  ></input>
+                  {errors.password && <p className='text-red-500 text-[0.5rem] italic'>{errors.password}</p>}
+                <FaLock className='absolute left-3 top-1/2 transform -translate-y-1/2 text-black'/>
+              </div>
+              <div className='relative mb-8'>
+                <input 
+                  type='password' 
+                  name= 'confirmPassword'
+                  placeholder = 'Enter your password again' 
+                  className='w-full bg-white rounded-md h-8 pl-15 pr-6 py-3 text-black'
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                  ></input>
+                  {errors.confirmPassword && <p className='text-red-500 text-[0.5rem] italic'>{errors.confirmPassword}</p>}
+                <FaLock className='absolute left-3 top-1/2 transform -translate-y-1/2 text-black'/>
+              </div>
+            </form>
+            <div className='flex w-full justify-center py-3 mb-2'>
+              <button 
+                className='border-2 w-1/2 border-white mb-2 rounded-lg px-2 py-2 justify-center hover:bg-white hover:text-black cursor-pointer'
+                type='submit'
+                onClick={handleSubmit}
+                >
+                  Register
+              </button>
+            </div>
+            {showPopup && <Popup message="Registration Successful!" onClose={() => setShowPopup(false)} />}
+        </div>
+      </div>
   )
 }
 
