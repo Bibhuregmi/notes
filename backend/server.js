3ea2ee1ab2a3e6d8a968f37d8ced01b14f52d3f5
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-03-18 14:19:25 -0400

    CORS issue resloved

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,17 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { connectDb } from './config/db.js';
 import routes from './routes/noteRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import cors from 'cors';
 const port = process.env.PORT
 const app = express(); 
 
 connectDb(); 
 
+//this will allow all origins to make request to the backend server
+app.use(cors({
+    origin: 'http://localhost:5000', //url of the frontend server
+    methods: ['POST', 'GET', 'PUT', 'DELETE']
+}));
 //using the middleware that parses the input body request
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
