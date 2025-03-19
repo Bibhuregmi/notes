dc56202063c44796052e601f8f7670b85c2084bc
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-03-19 17:43:55 -0400

    Use of auth context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/Routes'
+import { AuthProvider } from './context/AuthContext'
 const App = () => {
   return (
-    <RouterProvider router={router} /> 
+    <AuthProvider>
+      <RouterProvider router={router} /> 
+    </AuthProvider>
   )
 }
 
