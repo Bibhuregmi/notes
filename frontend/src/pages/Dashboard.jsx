40870e913c567959565a65e56c726eaf498d4788
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-03-19 17:40:59 -0400

    Fetch notes and conditional rendering according to the auth state

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+import { AuthContext } from '../context/AuthContext';
 
 const Dashboard = () => {
+
+  const[notes, setNotes] = useState(''); 
+  const[auth, setAuth] = useState(!!localStorage.getItem('authToken')); //checking the auth token upon the component mount
+  const token = localStorage.getItem('authToken', auth)
+    
+  const {isAuthenticated} = useContext(AuthContext)
+  useEffect (() => {
+    const fetchNotes = async () => {
+      try {
+        if(isAuthenticated){
+          const res = await fetch("http://localhost:8000/api/notes", {
+            method: 'GET', 
+            headers: {
+              'Content-type' : 'application/json',
+              'Authorization': `Bearer ${token}` //sending token in the header to get the notes related to the the user only
+            },
+          })
+          const data = await res.json(); 
+          console.log(data); 
+          if(!res.ok){
+            console.error(`Error on Http, ${res.status}`)
+          }
+        }else{
+          throw new Error('User not authenticated! Prompt to login')
+        }
+      } catch (error) {
+        console.error('Error fetching notes', error)
+      }
+    }
+    fetchNotes();
+  }, [isAuthenticated, token]) //anytime the state of the authentication or the token changes, the useEffect runs again. 
   return (
     <>
-      <h1>This is the main part to display the notes</h1>
-      <div>
-        
-      </div>
+      {!isAuthenticated &&(
+        <div className='max-w-full h-[50vh] flex flex-col items-center gap-4 mt-4 px-4 py-4'>
+          <h1 className='text-8xl font-bold mb-10'>Welcome to Notes.</h1> 
+          <p className='text-lg italic'>Only the registered users can interact with the notes </p>
+          <h3>Sign In with your existing account or Register to create new Account.</h3>
+        </div>
+      )}
     </>
   )
 }
