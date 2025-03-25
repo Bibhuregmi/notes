faa1d72ff7569d21326269047635003259330d62
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-03-24 22:02:52 -0400

    Moved the fetch to the seperate module

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,38 +1,27 @@
 import React, { useEffect, useState, useContext } from 'react'
 import { AuthContext } from '../context/AuthContext';
+import { getUserName, fetchNotes } from '../utils/apiCalls';
 
 const Dashboard = () => {
 
   const[notes, setNotes] = useState(''); 
+  const[userName, setUserName] = useState(''); 
   const[auth, setAuth] = useState(!!localStorage.getItem('authToken')); //checking the auth token upon the component mount
   const token = localStorage.getItem('authToken', auth)
     
   const {isAuthenticated} = useContext(AuthContext)
-  useEffect (() => {
-    const fetchNotes = async () => {
-      try {
-        if(isAuthenticated){
-          const res = await fetch("http://localhost:8000/api/notes", {
-            method: 'GET', 
-            headers: {
-              'Content-type' : 'application/json',
-              'Authorization': `Bearer ${token}` //sending token in the header to get the notes related to the the user only
-            },
-          })
-          const data = await res.json(); 
-          console.log(data); 
-          if(!res.ok){
-            console.error(`Error on Http, ${res.status}`)
-          }
-        }else{
-          throw new Error('User not authenticated! Prompt to login')
-        }
-      } catch (error) {
-        console.error('Error fetching notes', error)
+  
+  useEffect (() => { 
+    if(isAuthenticated){
+      const loadData = async () => {
+        const userName = await getUserName(); 
+        setUserName(userName); 
+        await fetchNotes(); 
       }
-    }
-    fetchNotes();
+      loadData(); 
+    } 
   }, [isAuthenticated, token]) //anytime the state of the authentication or the token changes, the useEffect runs again. 
+
   return (
     <>
       {!isAuthenticated &&(
@@ -40,6 +29,11 @@ const Dashboard = () => {
           <h1 className='text-8xl font-bold mb-10'>Welcome to Notes.</h1> 
           <p className='text-lg italic'>Only the registered users can interact with the notes </p>
           <h3>Sign In with your existing account or Register to create new Account.</h3>
+        </div>
+      )}
+      {isAuthenticated && (
+        <div>
+          {`Welcome to notes ${userName}`} 
         </div>
       )}
     </>
