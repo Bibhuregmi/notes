961df1920f6ad7e05d8e11bfd90661d66d749745
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-03-26 15:31:43 -0400

    Changed the token retrival to dynanic function

--- frontend/src/utils/apiCalls.js
+++ frontend/src/utils/apiCalls.js
@@ -1,6 +1,9 @@
-const token = localStorage.getItem('authToken');
+const getToken = () => {
+    return localStorage.getItem('authToken'); 
+}
 
 export const getUserName = async () => {
+    const token = getToken(); 
     try {
         const res = await fetch("http://localhost:8000/api/users/me", {
             method: 'GET', 
@@ -20,27 +23,29 @@ export const getUserName = async () => {
     }
 } 
 export const fetchNotes = async () => {
-try {
-    const res = await fetch("http://localhost:8000/api/notes", {
-        method: 'GET', 
-        headers: {
-        'Content-type' : 'application/json',
-        'Authorization': `Bearer ${token}` //sending token in the header to get the notes related to the the user only
-        },
-    })
-    const data = await res.json(); 
-    console.log(data); 
-    if(!res.ok){
-        console.error(`Error on Http, ${res.status}`)
-    }
-    return data; 
-    }catch (error) {
-    console.error('Error fetching notes', error)
-    return null; 
-    }
+    const token = getToken(); 
+    try {
+        const res = await fetch("http://localhost:8000/api/notes", {
+            method: 'GET', 
+            headers: {
+            'Content-type' : 'application/json',
+            'Authorization': `Bearer ${token}` //sending token in the header to get the notes related to the the user only
+            },
+        })
+        const data = await res.json(); 
+        console.log(data); 
+        if(!res.ok){
+            console.error(`Error on Http, ${res.status}`)
+        }
+        return data; 
+        }catch (error) {
+        console.error('Error fetching notes', error)
+        return null; 
+        }
 }
 
 export const deleteNotes = async (noteId) => {
+    const token = getToken();
     try{
         const res = await fetch(`http://localhost:8000/api/notes/${noteId}`, {
             method: 'DELETE', 
